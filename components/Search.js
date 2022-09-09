d012a7f3d2b6c16be4f54e83fe841684d1211243
Talha Shahid <talhashahidch@gmail.com>
2022-09-09 13:22:36 +0500

    search bar

--- components/Search.js
+++ components/Search.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import SearchedItems from './SearchedItems'
 
 
 const Search = (props) => {
@@ -7,7 +6,7 @@ const Search = (props) => {
     <>
     <form>
     <label
-      for="default-search"
+      htmlFor="default-search"
       className="mb-2 text-sm font-normal text-gray-900 sr-only dark:text-gray-300"
     >
       Search
@@ -23,9 +22,9 @@ const Search = (props) => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
           ></path>
         </svg>
--- components/SearchedItems.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SearchedItems = ({filteredCoins}) => {
-  return (
-    <div className='absolute top-30 right-20 bg-red-400 w-48 z-40'>{filteredCoins.map((coin) => (<ul><li>{coin.name}</li></ul>))}</div>
-  )
-}
-
-export default SearchedItems
